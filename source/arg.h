bb3434cc16d8f804b947bd81cb39996b33e53fed
thomas <gr@grrrr.org>
2002-07-26 23:46:51 +0000

    no message
    git-svn-id: https://svn.grrrr.org/ext/vasp/trunk@2560 4d9ac71a-51e6-0310-8455-cad1006bcd31

---
+++ source/arg.h
@@ -0,0 +1,93 @@
+/* 
+
+VASP modular - vector assembling signal processor / objects for Max/MSP and PD
+
+Copyright (c) 2002 Thomas Grill (xovo@gmx.net)
+For information on usage and redistribution, and for a DISCLAIMER OF ALL
+WARRANTIES, see the file, "license.txt," in this distribution.  
+
+*/
+
+#ifndef __VASP_ARG_H
+#define __VASP_ARG_H
+
+#include "main.h"
+
+class Vasp;
+
+class Argument
+{
+public:
+	Argument();
+	~Argument();
+	
+	Argument &Parse(I argc,t_atom *argv);
+	Argument &Clear();
+	Argument &ClearAll();
+
+	Argument &Set(Vasp *v);
+	Argument &Set(I argc,t_atom *argv);
+	Argument &Set(I i);
+	Argument &Set(F f);
+	Argument &Set(D d);
+	Argument &Set(F re,F im);
+	Argument &Set(VX *vec);
+
+	Argument *Next() { return nxt; }
+	Argument &Next(I i);
+	Argument &Add(Argument *a);
+
+	Argument &Add(Vasp *v);
+	Argument &Add(I argc,t_atom *argv);
+	Argument &Add(I i);
+	Argument &Add(F f);
+	Argument &Add(D d);
+	Argument &Add(F re,F im);
+	Argument &Add(VX *vec);
+
+	BL IsNone() const { return tp == tp_none; }
+	BL IsVasp() const { return tp == tp_vasp; }
+	BL IsList() const { return tp == tp_list; }
+	BL IsInt() const { return tp == tp_int; }
+	BL CanbeInt() const { return tp == tp_int || tp == tp_float || tp_double; }
+	BL IsFloat() const { return tp == tp_float; }
+	BL CanbeFloat() const { return tp == tp_float || tp == tp_double || tp == tp_int; }
+	BL IsDouble() const { return tp == tp_double; }
+	BL CanbeDouble() const { return tp == tp_double || tp == tp_float || tp == tp_int; }
+	BL IsComplex() const { return tp == tp_cx; }
+	BL CanbeComplex() const { return tp == tp_cx || CanbeFloat(); }
+	BL IsVector() const { return tp == tp_vx; }
+	BL CanbeVector() const { return tp == tp_vx || CanbeComplex(); }
+
+	const Vasp &GetVasp() const { return *dt.v; }
+	const AtomList &GetList() const { return *dt.atoms; }
+	I GetInt() const { return dt.i; }
+	I GetAInt() const;
+	F GetFloat() const { return dt.f; }
+	F GetAFloat() const;
+	D GetDouble() const { return dt.d; }
+	D GetADouble() const;
+	const CX &GetComplex() const { return *dt.cx; }
+	CX GetAComplex() const;
+	const VX &GetVector() const { return *dt.vx; }
+	VX GetAVector() const;
+
+protected:
+	enum {
+		tp_none,tp_vasp,tp_list,tp_int,tp_float,tp_double,tp_cx,tp_vx
+	} tp;
+
+	union {
+		Vasp *v;
+		AtomList *atoms;
+		F f;
+		D d;
+		I i;
+		CX *cx;
+		VX *vx;
+	} dt;
+
+	Argument *nxt;
+};
+
+#endif
