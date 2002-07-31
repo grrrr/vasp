7e446737e9ad90eda49442fd1f101f3f876d37c8
thomas <gr@grrrr.org>
2002-07-31 20:08:09 +0000

    no message
    git-svn-id: https://svn.grrrr.org/ext/vasp/trunk@2573 4d9ac71a-51e6-0310-8455-cad1006bcd31

---
+++ source/env.h
@@ -0,0 +1,25 @@
+/* 
+
+VASP modular - vector assembling signal processor / objects for Max/MSP and PD
+
+Copyright (c) 2002 Thomas Grill (xovo@gmx.net)
+For information on usage and redistribution, and for a DISCLAIMER OF ALL
+WARRANTIES, see the file, "license.txt," in this distribution.  
+
+*/
+
+#ifndef __VASP_BPTS_H
+#define __VASP_BPTS_H
+
+#include "vasp.h"
+
+class Bpts
+{
+public:
+	Bpts(I argc,t_atom *argv);
+	~Bpts();
+
+};
+
+#endif
+
