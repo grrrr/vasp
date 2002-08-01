3b5b39b28eeffe56d5665f7b33cd9c0aecd14bfe
thomas <gr@grrrr.org>
2002-08-01 18:08:40 +0000

    no message
    git-svn-id: https://svn.grrrr.org/ext/vasp/trunk@2579 4d9ac71a-51e6-0310-8455-cad1006bcd31

---
+++ source/buflib.h
@@ -0,0 +1,25 @@
+/* 
+
+VASP modular - vector assembling signal processor / objects for Max/MSP and PD
+
+Copyright (c) 2002 Thomas Grill (xovo@gmx.net)
+For information on usage and redistribution, and for a DISCLAIMER OF ALL
+WARRANTIES, see the file, "license.txt," in this distribution.  
+
+*/
+
+/*! \file buflib.h
+	\brief Routines for buffer management
+*/
+
+#ifndef __VASP_BUFLIB_H
+#define __VASP_BUFLIB_H
+
+namespace BufLib
+{
+	VBuffer *Get(t_symbol *s);
+};
+
+
+
+#endif
