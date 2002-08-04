ea96bf18e6eb3044b1797a0c76206da899920988
thomas <gr@grrrr.org>
2002-08-04 21:39:38 +0000

    no message
    git-svn-id: https://svn.grrrr.org/ext/vasp/trunk@2586 4d9ac71a-51e6-0310-8455-cad1006bcd31

---
+++ source/obj_radio.cpp
@@ -0,0 +1,83 @@
+/* 
+
+VASP modular - vector assembling signal processor / objects for Max/MSP and PD
+
+Copyright (c) 2002 Thomas Grill (xovo@gmx.net)
+For information on usage and redistribution, and for a DISCLAIMER OF ALL
+WARRANTIES, see the file, "license.txt," in this distribution.  
+
+*/
+
+/*! \file obj_radio.cpp
+	\brief objects for radio messages.
+*/
+
+
+#include "classes.h"
+
+/*! \class vasp_radio
+	\remark \b vasp.radio
+	\brief Lets only radio messages pass through.
+	\since 0.0.6
+	\param inlet.1 * - any message
+	\retval outlet radio messages
+*/
+class vasp_radio:
+	public flext_base
+{
+	FLEXT_HEADER(vasp_radio,flext_base)
+
+public:
+
+	vasp_radio()
+	{
+		AddInAnything();
+		AddOutAnything();
+		SetupInOut();
+
+		FLEXT_ADDMETHOD_A(0,m_any);
+	}
+
+	virtual V m_any(t_symbol *s,I argc,t_atom *argv);
+
+	virtual V m_help() { post("%s - lets only radio messages pass through",thisName()); }
+private:
+	FLEXT_CALLBACK_A(m_all);
+};
+
+FLEXT_LIB("vasp.radio",vasp_radio)
+
+
+V vasp_radio::m_any(t_symbol *s,I argc,t_atom *argv) 
+{
+	if(s == sym_radio) ToOutAnything(s,argc,argv);
+}
+
+
+
+/*! \class vasp_noradio
+	\remark \b vasp.!radio
+	\brief Filters out radio messages 
+	\since 0.0.6
+	\param inlet.1 * - any message
+	\retval outlet all messages except radio messages
+*/
+class vasp_noradio:
+	public flext_radio
+{
+	FLEXT_HEADER(vasp_noradio,vasp_radio)
+
+public:
+
+	virtual V m_any(t_symbol *s,I argc,t_atom *argv);
+
+	virtual V m_help() { post("%s - filters out all radio messages",thisName()); }
+};
+
+FLEXT_LIB("vasp.noradio vasp.!radio",vasp_noradio)
+
+
+V vasp_noradio::m_any(t_symbol *s,I argc,t_atom *argv) 
+{
+	if(s != sym_radio) ToOutAnything(s,argc,argv);
+}
