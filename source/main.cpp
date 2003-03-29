83f56246912e93e85e82008ce89a191ddd7ef0ea
thomas <gr@grrrr.org>
2003-03-29 10:57:15 +0000

    no message
    git-svn-id: https://svn.grrrr.org/ext/vasp/trunk@2913 4d9ac71a-51e6-0310-8455-cad1006bcd31

--- source/main.cpp
+++ source/main.cpp
@@ -12,7 +12,7 @@ WARRANTIES, see the file, "license.txt," in this distribution.
 #include "classes.h"
 
 
-const C *VASP_VERSION = "0.1.3pre5";
+const C *VASP_VERSION = "0.1.3pre6";
 
 #include "opfuns.h"
 
